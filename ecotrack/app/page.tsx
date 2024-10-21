26f1827ac7dfb3f2a976746c9acb6484cca2df02
haile <haileleulfiseha@gmail.com>
2024-10-21 23:29:24 +0300

    second section of the home page , feature cards are added and displayed

--- ecotrack/app/page.tsx
+++ ecotrack/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
 				<h1 className="text-6xl font-bold mb-6 text-gray-800 tracking-tight">
 					Eco-Track
 					<span className="text-green-600 ml-2">
-						Smarter Waste Management
+						Smart Waste Management
 					</span>
 				</h1>
 				<p className="text-xl text-gray-600 max-w-2xl mx-auto leading-relaxed ">
@@ -45,6 +45,44 @@ export default function Home() {
 					Report Waste
 				</Button>
 			</section>
+
+			<section className="grid md:grid-cols-3 gap-10 mb-20">
+				<FeatureCard
+					icon={Leaf}
+					title="Eco-Friendly"
+					description="Dedicated to reducing waste and fostering sustainability."
+				/>
+				<FeatureCard
+					icon={Coins}
+					title="Earn Rewards"
+					description="Get rewarded for helping the environment through sustainable practices."
+				/>
+				<FeatureCard
+					icon={Users}
+					title="Community Driven"
+					description="Join us in building a sustainable, waste-conscious community."
+				/>
+			</section>
+		</div>
+	);
+}
+
+function FeatureCard({
+	icon: Icon,
+	title,
+	description,
+}: {
+	icon: React.ElementType;
+	title: string;
+	description: string;
+}) {
+	return (
+		<div className="bg-white p-8  rounded-xl shadow-md hover:shadow-lg transition-all duration-300 ease-in-out flex flex-col items-center text-center ">
+			<div className="bg-green-100 p-4 rounded-full mb-6">
+				<Icon className="h-8 w-8 text-green-600" />
+			</div>
+			<h3 className="text-xl font-semibold mb-4 text=gray-800">{title}</h3>
+			<p className="text=gray-600 leading-relaxed ">{description}</p>
 		</div>
 	);
 }
